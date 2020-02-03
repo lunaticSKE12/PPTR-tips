7177a2e12f45afeeff21067791e6969f2b813219
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-03 23:30:04 +0700

    Accessibility Test and web performance

--- example.js
+++ example.js
@@ -2,38 +2,26 @@ const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 
 (async () => {
-  // screenshot
-  // const browser = await puppeteer.launch()
-  // const page = await browser.newPage()
-  // await page.goto('https://www.example.com', { waitUntil: 'networkidle0' })
-  // await page.screenshot({ path: 'example.png', fullPage: true })
-  // await browser.close()
-
-  // convert page tp PDF file
-  // const browser = await puppeteer.launch()
-  // const page = await browser.newPage()
-  // await page.goto('https://www.example.com', { waitUntil: 'networkidle0' })
-  // await page.pdf({ path: 'example.pdf', format: 'A4' })
-  // await browser.close()
-
-  // Emulating devices
+  // Accessibility Test
   // const browser = await puppeteer.launch({ headless: false })
   // const page = await browser.newPage()
-  // await page.emulate(devices['iPhone X'])
-  // await page.goto('https://pptr.dev')
-  // await page.waitFor(10000)
-  // await page.close()
 
-  // Emulating devices
+  // await page.goto('https://pptr.dev')
+  // await page.waitForSelector('title')
+
+  // const snapshot = await page.accessibility.snapshot()
+  // console.log(snapshot)
+  // await browser.close()
+
+  // Accessibility Test
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
-  // Grant permissions for geolocation change
-  const context = browser.defaultBrowserContext()
-  await context.overridePermissions('https://pptr.dev', ['geolocation'])
+
   await page.goto('https://pptr.dev')
   await page.waitForSelector('title')
 
-  // Change geolocation to the north pole
-  await page.setGeolocation({ latitude: 90, longitude: 0 })
-  await page.close()
+  // Execute Navigation API within the page context
+  const metrics = await page.evaluate(() => JSON.stringify(window.performance))
+  console.log(JSON.parse(metrics))
+  await browser.close()
 })();
