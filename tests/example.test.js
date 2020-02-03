d7689a661219b10883603c741de5a542191a151e
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-04 00:41:02 +0700

    es6 class

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,11 +1,14 @@
 // const puppeteer = require(puppeteer)
 import puppeteer from 'puppeteer'
+import Homepage from './homepage'
 
 describe('Loads URL', () => {
   it('should work', async () => {
     const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
-    await page.goto('http://zero.webappsecurity.com/index.html')
+    const homepage = new Homepage(page)
+    // await page.goto('http://zero.webappsecurity.com/index.html')
+    await homepage.visit()
     await page.waitFor(5000)
     await browser.close()
   })
