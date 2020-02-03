fe890fe38d9817ba5cbaaf64e21e4a50dd6f9fc3
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-03 23:16:12 +0700

    Change geolocation

--- example.js
+++ example.js
@@ -17,10 +17,23 @@ const devices = require('puppeteer/DeviceDescriptors');
   // await browser.close()
 
   // Emulating devices
+  // const browser = await puppeteer.launch({ headless: false })
+  // const page = await browser.newPage()
+  // await page.emulate(devices['iPhone X'])
+  // await page.goto('https://pptr.dev')
+  // await page.waitFor(10000)
+  // await page.close()
+
+  // Emulating devices
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
-  await page.emulate(devices['iPhone X'])
+  // Grant permissions for geolocation change
+  const context = browser.defaultBrowserContext()
+  await context.overridePermissions('https://pptr.dev', ['geolocation'])
   await page.goto('https://pptr.dev')
-  await page.waitFor(10000)
+  await page.waitForSelector('title')
+
+  // Change geolocation to the north pole
+  await page.setGeolocation({ latitude: 90, longitude: 0 })
   await page.close()
 })();
