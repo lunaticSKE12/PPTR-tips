4f5a2097ff5c818c3cb681bd3fe426d3c2468007
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-03 23:54:57 +0700

    Incognito mode

--- example.js
+++ example.js
@@ -2,26 +2,13 @@ const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 
 (async () => {
-  // Accessibility Test
-  // const browser = await puppeteer.launch({ headless: false })
-  // const page = await browser.newPage()
-
-  // await page.goto('https://pptr.dev')
-  // await page.waitForSelector('title')
-
-  // const snapshot = await page.accessibility.snapshot()
-  // console.log(snapshot)
-  // await browser.close()
-
-  // Accessibility Test
+  // Incognito
   const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
+  const context = await browser.createIncognitoBrowserContext()
+  const page = await context.newPage()
 
-  await page.goto('https://pptr.dev')
-  await page.waitForSelector('title')
+  await page.goto('http://zero.webappsecurity.com/index.html')
+  await page.waitFor(5000)
 
-  // Execute Navigation API within the page context
-  const metrics = await page.evaluate(() => JSON.stringify(window.performance))
-  console.log(JSON.parse(metrics))
   await browser.close()
 })();
