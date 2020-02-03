7174a3f02c8e0cff6f3bf537896ab747722b25ac
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-03 23:11:39 +0700

    Page to pdf and page devices

--- example.js
+++ example.js
@@ -1,9 +1,26 @@
 const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
 
 (async () => {
-  const browser = await puppeteer.launch()
+  // screenshot
+  // const browser = await puppeteer.launch()
+  // const page = await browser.newPage()
+  // await page.goto('https://www.example.com', { waitUntil: 'networkidle0' })
+  // await page.screenshot({ path: 'example.png', fullPage: true })
+  // await browser.close()
+
+  // convert page tp PDF file
+  // const browser = await puppeteer.launch()
+  // const page = await browser.newPage()
+  // await page.goto('https://www.example.com', { waitUntil: 'networkidle0' })
+  // await page.pdf({ path: 'example.pdf', format: 'A4' })
+  // await browser.close()
+
+  // Emulating devices
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
-  await page.goto('https://www.example.com', { waitUntil: 'networkidle0' })
-  await page.screenshot({ path: 'example.png', fullPage: true })
-  await browser.close()
+  await page.emulate(devices['iPhone X'])
+  await page.goto('https://pptr.dev')
+  await page.waitFor(10000)
+  await page.close()
 })();
